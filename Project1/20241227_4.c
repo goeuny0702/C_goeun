b0c94459f5f84d478af0c038973105d306ed2ebd
[goeun] <[zz4442@naver.com]>
2024-12-27 15:46:26 +0900

    gg

--- Project1/20241227_4.c
+++ Project1/20241227_4.c
@@ -1,5 +1,13 @@
 #include<stdio.h>
 
 main() {
+	int x, y;
+	for (x = 2; x <= 9; x++) {
+		printf("***** %d\n", x, "´Ü *****");
+		for (y = 1; y <= x; y++) {
+			printf("x X y = %d\n", x * y);
+		}
+
+	}
 
 }
